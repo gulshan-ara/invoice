97c48c6867d340267c3501908ecc496cc516641c
gulshan-ara <gulshan3015@gmail.com>
2022-10-08 22:47:10 +0600

    View engine added for dynamic templating

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // require express for using it
 const express = require("express");
 const app = express();
+const path = require('path');
+const engine = require('ejs-blocks');
 
 // set the port
 const port = 3000;
 
+// tell app to use ejs 
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
+app.set('views', path.join(__dirname, '/views'));
+
 // bind the middleware
-app.use((req, res) => {
-    res.send(`<h1> Welcome to your express app! </h1>`);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// adding the view engine
+app.get('/', (req, res) => {
+    // rendering the ejs file
+    res.render('home', { style: "css/home.css" });
 });
 
 // bind and listen the connections on the specified host and port
