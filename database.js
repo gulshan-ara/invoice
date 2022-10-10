fb793f344a375a688633493df1d5c5890126b1e7
gulshan-ara <gulshan3015@gmail.com>
2022-10-10 11:17:19 +0600

    Update operation implemented in Customer table

--- database.js
+++ database.js
@@ -14,19 +14,21 @@ con.connect(function(err) {
 
     console.log(`Database is connected successfully !`);
 
-    const create_customer = 
-    `CREATE TABLE if not exists Customer(
-    loginId INT AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    phone CHAR(20) NOT NULL,
-    PRIMARY KEY (loginId) 
+    const create_customer = `
+            CREATE TABLE if not exists Customer(
+            loginId INT AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            phone CHAR(20) NOT NULL,
+            PRIMARY KEY (loginId) 
     )`;
 
-    const create_invoice = 
-    `CREATE TABLE if not exists Invoice(
-    invoiceId INT AUTO_INCREMENT,
-    products JSON NOT NULL,
-    PRIMARY KEY (invoiceID) 
+    const create_invoice = `
+            CREATE TABLE if not exists Invoice(
+            invoiceId INT AUTO_INCREMENT,
+            customerName VARCHAR(255) NOT NULL,
+            invoiceDate CHAR(11) NOT NULL,
+            products JSON NOT NULL,
+            PRIMARY KEY (invoiceID) 
     )`;
 
     con.query(create_customer, function(err, results, fields) {
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const engine = require('ejs-blocks');
 const bodyParser = require('body-parser');
 
 const db = require('./database');
+const con = require("./database");
 
 
 // set the port
@@ -24,8 +25,22 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
+app.get('/value', function(req, res) {
+    res.render('value');
+});
+
 app.get('/Invoice', function(req, res) {
-    res.render('invoice');
+    const readQuery = `
+            SELECT * FROM Customer;
+    `;
+
+    db.query(readQuery, function(err, result) {
+        if (err) throw err;
+        else {
+            obj = { print: result };
+            res.render("invoice", obj);
+        }
+    })
 });
 
 // app.get('/Customers', function(req, res){
@@ -33,7 +48,11 @@ app.get('/Invoice', function(req, res) {
 // });
 
 app.get('/Customers', function(req, res) {
-    db.query("select * from Customer", function(err, result) {
+    const readQuery = `
+            SELECT * FROM Customer;
+    `;
+
+    db.query(readQuery, function(err, result) {
         if (err) throw err;
         else {
             obj = { print: result };
@@ -60,6 +79,66 @@ app.post('/save', function(req, res) {
         }
     });
 
+});
+
+let edit = 0;
+app.get('/update', function(req, res) {
+    const updateQuery = `SELECT * FROM Customer WHERE loginId = ${[req.query.id]};`;
+
+    db.query(updateQuery, function(error, rows, result) {
+        if (!!error) {
+            console.log('edit Error' + error);
+        } else {
+            console.log('edit ok');
+            obj = { print: rows };
+            console.log(obj);
+            res.render("updateCustomer", obj);
+        }
+    });
+});
+
+
+app.post('/updatesave', function(req, res) {
+    const id = req.body.id;
+    const name = req.body.name;
+    const contact = req.body.contact;
+
+    // res.send(`${name} and ${contact}`);
+
+    // update statment
+    let sql = `
+        UPDATE Customer
+        SET name = ?, phone = ?
+        WHERE loginId = ?
+    `;
+
+    let data = [name, contact, id];
+
+    // execute the UPDATE statement
+    db.query(sql, data, (error, results, fields) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log('Rows affected:', results.affectedRows);
+        res.redirect('/Customers');
+    });
+
+});
+
+app.get('/delete', function(req, res) {
+    const id = req.query.id;
+    const del_query = `
+            DELETE FROM Customer WHERE loginId="${id}";`
+
+    db.query(del_query, function(err, rows, fields) {
+
+        if (!!err) {
+            console.log('Error', +err);
+        } else {
+            console.log("record deleted");
+            return res.redirect('/Customers');
+        }
+    });
 });
 
 // bind and listen the connections on the specified host and port
